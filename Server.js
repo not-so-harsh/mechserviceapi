b8bf8b36afc02b751dc8c34b7fb46e659f770c3a
unknown <2016harsh2016@gmail.com>
2022-10-12 22:25:11 +0530

    changes

--- Server.js
+++ Server.js
@@ -2,5 +2,12 @@ const http = require('http');
 const port = process.env.PORT || 3001;
 const app = require('./App');
 const server =http.createServer(app);
+const cors = require('cors');
+
+
+app.use(cors());
+
+    
+
 
 server.listen(port,()=>(console.log('app is running :'+port)));
