9d3b463f018734e6069e3cfe3346e24e97dd35c6
unknown <2016harsh2016@gmail.com>
2022-10-21 11:12:50 +0530

    error fixed

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/user', userRoute)
+app.use('/user', userRoute) ;
 
 
 app.use((req, res, next) => {
